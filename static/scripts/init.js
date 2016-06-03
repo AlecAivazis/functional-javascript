5347ba4710c0d7b5ace32e909263923a5ba28b7d
Alec Aivazis <alec@aivazis.com>
2016-06-02 23:08:01 -0700

    syntax highlighting is done with prism

--- static/scripts/init.js
+++ static/scripts/init.js
@@ -2,6 +2,9 @@ Reveal.initialize({
     history: true,
     dependencies: [
         // Syntax highlight for <code> elements
-        { src: 'static/lib/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        // { src: 'static/lib/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        // prism instead of highlight
+        { src: 'static/lib/plugin/prism/prism.js', async: false},
+        { src: 'static/scripts/initPrism.js', async: false},
     ]
 })
