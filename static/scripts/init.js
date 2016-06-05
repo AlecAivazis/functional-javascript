b3d15803f6d0331242047f5295773e4d606bda67
Alec Aivazis <alec@aivazis.com>
2016-06-05 12:42:47 -0700

    added fragments to es6 slides

--- static/scripts/init.js
+++ static/scripts/init.js
@@ -6,6 +6,7 @@ Reveal.initialize({
         // { src: 'static/lib/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
         // prism instead of highlight
         { src: 'static/lib/plugin/prism/prism.js', async: false},
+        { src: 'static/lib/plugin/prism/prism.js', async: false},
         { src: 'static/scripts/initPrism.js', async: false},
     ]
 })
